463cb460759c7aac7c4f345dd1f0d61aa49c9367
tostipolitics <jstrujillo.andes@gmail.com>
2020-10-26 11:50:37 -0500

    R PROGRAMMING ASSIGNMENT WEEK 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,11 +1,23 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Catching the inverse of a matrix
+
 
 makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+      x <<- y
+      m <<- NULL
+    }
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setiverse,
+         getiverse = getinverse)
+  }
 
-}
 
 
 ## Write a short comment describing this function
